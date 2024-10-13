ec941cb4fca38eb556242fc6a54dd4663382fa1e
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-10-13 09:48:18 +0530

    feat: create the home page layout and design

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import styles from "./about.module.css"
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div>
+      <div className={styles.imageContainer}>
+        <Image src="/about.png" width={500} height={500} />
+      </div>
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
 import Links from "./links/Links";
 import styles from "./navbar.module.css";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Logo</div>
+      <Link href="/" className={styles.logo}>
+        Blog
+      </Link>
       <div>
         <Links />
       </div>
