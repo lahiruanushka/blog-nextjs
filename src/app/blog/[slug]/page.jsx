478624800554a2fd4ab6155ca58450edd951b689
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-10-13 11:23:28 +0530

    design: create and style single post page layout

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,56 @@
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+
 const SinglePostPage = () => {
   return (
-    <div>SinglePostPage</div>
-  )
-}
+    <div className={styles.container}>
+      {/* Dummy image data */}
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/27163466/pexels-photo-27163466/free-photo-of-woman-reading-a-book-in-shadow.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+          alt="Post Image"
+          fill
+          className={styles.img}
+        />
+      </div>
 
-export default SinglePostPage
+      <div className={styles.textContainer}>
+        {/* Dummy title */}
+        <h1 className={styles.title}>Sample Post Title</h1>
+
+        <div className={styles.detail}>
+          <div className={styles.authorImageContainer}>
+            <Image
+              src="https://images.pexels.com/photos/27163466/pexels-photo-27163466/free-photo-of-woman-reading-a-book-in-shadow.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+              alt="Post Image"
+              height={50}
+              width={50}
+              className={styles.authorImage}
+            />
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Jhon Snow</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>
+              {/* Dummy date */}
+              {new Date().toString().slice(4, 16)}
+            </span>
+          </div>
+        </div>
+
+        {/* Dummy content description */}
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit
+          amet accumsan arcu. Sed vulputate consequat leo, ac elementum odio
+          gravida at. Phasellus euismod, neque vitae feugiat convallis, massa
+          neque tincidunt libero, eget convallis nisi elit ut magna.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SinglePostPage;
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
+import Image from "next/image";
 
 const links = [
   { title: "Home", path: "/" },
@@ -36,12 +37,14 @@ const Links = () => {
       </div>
 
       {/* Mobile menu button */}
-      <button
+      <Image
         className={styles.menuButton}
+        src="/menu.png"
+        alt=""
+        width={30}
+        height={30}
         onClick={() => setOpen((prev) => !prev)}
-      >
-        Menu
-      </button>
+      />
 
       {/* Show mobile links when open */}
       {open && (
